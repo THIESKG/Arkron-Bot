527788fb195f4dc0a5daa41aba0b336ebab024b2
THIESKG <thiesdoelen@gmail.com>
2020-11-16 17:19:39 +0100

    Update lore.js

--- commands/lore.js
+++ commands/lore.js
@@ -1,5 +1,5 @@
 module.exports = {
-    name: 'help',
+    name: 'lore',
     description: 'The help command.',
     execute(message, args) {
         message.channel.send('The Lore of Arkron: \n https://sites.google.com/view/arkronguide/lore?authuser=0')
