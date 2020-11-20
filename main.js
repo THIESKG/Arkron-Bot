67e4dc2a43f90b799b415c841eb379c94c6133de
THIESKG <thiesdoelen@gmail.com>
2020-11-20 14:05:09 +0100

    Update main.js

--- main.js
+++ main.js
@@ -19,6 +19,15 @@ client.once('ready', () => {
     console.log('Bot is Online.')
 });
 
+Client.on('guildMemberAdd', member =>{
+
+    const channel = member.guild.channels.find(channel => channel.name === "hello-and-bye-bye")
+    if(!channel) return;
+
+    channel.send(`Welcome to Arkron ${member}\n\nWe have one daily mandatory event hosted in #announcements based primarily on Ilum at the moment, and we are doing some jedi sanctum on the side. \n\nWe have non mandatory events hosted in #events, you can find the schedule for them in #events-schedule.\n\nTo find out pretty much everything about Arkron you can view #guides and rules are located in #rules. \n\nIf your wondering why the chats only go back a couple weeks it’s because our old discord was AAed. Someone gained acces to a HRs alt and deleted all the channels. Instead of carrying on there we deduced to create and entirely new discord.`)
+
+})
+
 client.on('message', message => {
     if(!message.content.startsWith(prefix) || message.author.bot) return;
 
